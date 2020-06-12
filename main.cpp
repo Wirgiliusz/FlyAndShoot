99dc480e7438505164c75e92ca1556e4b3107b40
Wirgiliusz <kamx98@gmail.com>
2020-06-12 15:32:12 +0200

    Dodanie ikony w oknie

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/icons/flyandshoot_icon.ico"));
     MainWindow w;
     w.show();
     return a.exec();
